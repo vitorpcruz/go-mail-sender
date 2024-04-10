a56e2c0bc3d824fa2933a09509d313cfb520a05e
Vitor P Cruz <vitor-pcruz@hotmail.com>
2024-04-10 17:57:29 -0300

    implementing handler errors test

---
+++ internal/endpoints/handler_error_test.go
@@ -0,0 +1,29 @@
+package endpoints_test
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"go-mail-sender/internal/endpoints"
+	"go-mail-sender/internal/internal_errors"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_HanderError_WhenEndpointReturnsInternalError(t *testing.T) {
+	assert := assert.New(t)
+
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return nil, 0, internal_errors.ErrInternal
+	}
+
+	handlerFunc := endpoints.HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+
+	assert.Equal(http.StatusInternalServerError, res.Code)
+	assert.Contains(res.Body.String(), internal_errors.ErrInternal.Error())
+}
