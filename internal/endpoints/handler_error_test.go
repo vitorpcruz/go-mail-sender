0021b19ddf6d8c0a978321042c2384b60238f6ee
Vitor P Cruz <vitor-pcruz@hotmail.com>
2024-04-10 18:15:06 -0300

    testing handler error when returning non 4xx/5xx code + obj content

--- internal/endpoints/handler_error_test.go
+++ internal/endpoints/handler_error_test.go
@@ -1,6 +1,7 @@
 package endpoints_test
 
 import (
+	"encoding/json"
 	"errors"
 	"net/http"
 	"net/http/httptest"
@@ -44,4 +45,31 @@ func Test_HanderError_WhenEndpointReturnsDomainError(t *testing.T) {
 
 	assert.Equal(http.StatusBadRequest, res.Code)
 	assert.Contains(res.Body.String(), "domain error")
+}
+
+func Test_HanderError_WhenEndpointReturnsObjAndStatus(t *testing.T) {
+	assert := assert.New(t)
+
+	type BodyForTest struct {
+		ID int
+	}
+
+	objExpected := BodyForTest{ID: 2}
+
+	endpoint := func(w http.ResponseWriter, r *http.Request) (interface{}, int, error) {
+		return objExpected, 201, nil
+	}
+
+	handlerFunc := endpoints.HandlerError(endpoint)
+	req, _ := http.NewRequest("GET", "/", nil)
+	res := httptest.NewRecorder()
+
+	handlerFunc.ServeHTTP(res, req)
+
+	objReturned := BodyForTest{}
+	json.Unmarshal(res.Body.Bytes(), &objReturned)
+
+	assert.Equal(http.StatusCreated, res.Code)
+
+	assert.Equal(objExpected, objReturned)
 }
